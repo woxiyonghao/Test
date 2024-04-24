330bf26d236a1b9389a635f317aedc2c7dd7f29f
woxiCLS <woxiyonghao@live.com>
2024-04-24 20:46:23 +0800

    first

---
+++ TestYML/TestYML/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  TestYML
+//
+//  Created by 卢瑟斯 on 2024/4/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestYML/TestYML/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  TestYML
+//
+//  Created by 卢瑟斯 on 2024/4/24.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
